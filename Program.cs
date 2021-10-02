be4f6a9737c90e5440b9a290a1cf9cd006bf755f
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-10-03 02:11:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HangManGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            Hangman game = new Hangman(); //สร้าง object game จากคลาส Hangman
+            Console.WriteLine("Welcome to Hangman Game");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1. Play game\n2. Exit"); //แสดงตัวเลือกให้ผู้ใช้เลือก
+            Console.Write("Please Select Menu : ");
+            menu = Convert.ToInt32(Console.ReadLine()); //รับค่าตัวเลือกใส่ไว้ในตัวแปร menu
+
+            if(menu == 1) //ถ้าผู้ใช้เลือก 1
+            {
+                game.play(); //เรียกใช้ method play() ในคลาส Hangman
+            }
+        }
+    }
+}
